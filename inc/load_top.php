0e27fb31cf27171f0c63f8d917c4a0b8180aa727
femmy_san <femmy.pramana@gmail.com>
2017-03-17 17:21:47 +0700

    first hit

---
+++ inc/load_top.php
@@ -0,0 +1,5 @@
+<link rel="icon" href="favicon.ico" type="image/x-icon">
+<link href="https://fonts.googleapis.com/css?family=Raleway:400,700,900" rel="stylesheet" type="text/css">
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" type="text/css">
+<link href="assets/vendors/bootstrap/dist/css/bootstrap.css" rel="stylesheet">
+<link href="assets/css/style.css" rel="stylesheet">
