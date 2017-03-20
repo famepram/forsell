23dfaa19a4b7b77e35340ad52ba06617e6d9b6bb
femmy_san <femmy.pramana@gmail.com>
2017-03-20 16:57:59 +0700

    latest

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+	$('.navbar-toggler').click(function(){
+		if($('body').hasClass('overlay-show')){
+			$('.sidebar-overlay').fadeOut('fast');
+			$('body').removeClass('overlay-show');
+		} else {
+			$('.sidebar-overlay').fadeIn('fast');
+			$('body').addClass('overlay-show');
+
+		}
+	});
+
+	$('body').on('click','.sidebar-overlay',function(){
+		if($('body').hasClass('overlay-show')){
+			$('.sidebar-overlay').fadeOut('fast');
+			$('body').removeClass('overlay-show');
+		} else {
+			$('.sidebar-overlay').fadeIn('fast');
+			$('body').addClass('overlay-show');
+		}
+	});
+});
---
+++ inc/load_bottom.php
@@ -0,0 +1,3 @@
+<script src="assets/vendors/jquery/dist/jquery.js" ></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" ></script>
+<script src="assets/js/script.js"></script>
