cb3172031ab38d0ee6d2142ae458920fb1610d27
femmy_san <femmy.pramana@gmail.com>
2017-03-21 16:48:13 +0700

    latst

--- index.php
+++ index.php
@@ -50,6 +50,23 @@
 						<h5>Administrator</h5>
 					</div>
 				</div>
+				<div class="menu">
+			        <ul class="nav nav-pills flex-column">
+			            <li class="nav-item">
+			              	<a class="nav-link active" href="#">Overview <span class="sr-only">(current)</span></a>
+			            </li>
+			            <li class="nav-item">
+			              	<a class="nav-link" href="#">Reports</a>
+			            </li>
+			            <li class="nav-item">
+			              	<a class="nav-link" href="#">Analytics</a>
+			            </li>
+			            <li class="nav-item">
+			              	<a class="nav-link" href="#">Export</a>
+			            </li>
+		          	</ul>
+			    </div>
+
 			</aside>
 		</section>
 		
