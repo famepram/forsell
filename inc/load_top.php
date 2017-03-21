24a8a8eeffea9fb1e9f658b920327d373dc54593
femmy_san <femmy.pramana@gmail.com>
2017-03-21 15:47:41 +0700

    latest

--- inc/load_top.php
+++ inc/load_top.php
@@ -2,4 +2,5 @@
 <link href="https://fonts.googleapis.com/css?family=Raleway:400,700,900" rel="stylesheet" type="text/css">
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" type="text/css">
 <link href="assets/vendors/bootstrap/dist/css/bootstrap.css" rel="stylesheet">
+<link href="assets/vendors/zmdi/css/material-design-iconic-font.min.css" rel="stylesheet">
 <link href="assets/css/style.css" rel="stylesheet">
--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 		<meta charset="UTF-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=Edge">
     	<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-    	<title>Welcome To | Bootstrap Based Admin Template - Material Design</title>
+    	<title>FIXED ASSETS | DRAFT UI DEVELOPMENT</title>
     	
     	<?php include('inc/load_top.php');?>
 	</head>
@@ -13,12 +13,43 @@
 			<button class="navbar-toggler navbar-toggler-left" aria-controls="sidebar-left" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
 			</button>
-			<a class="navbar-brand" href="#">Dashboard</a>
+			<a class="navbar-brand" href="#">FIXED ASSETS</a>
+
+			<div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+				<ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+					<li class="nav-item active">
+						<a href=""><i class="zmdi zmdi-notifications"></i></a>
+					</li>
+					<li class="nav-item active">
+						<a href=""><i class="zmdi zmdi-search"></i></a>
+					</li>
+					<li class="nav-item active">
+						<a href=""><i class="zmdi zmdi-account-circle"></i></a>
+					</li>
+					<li class="nav-item active">
+						<a href=""><i class="zmdi zmdi-more-vert"></i></a>
+					</li>
+					
+				</ul>
+<!-- 				<form class="form-inline my-2 my-lg-0">
+					<input class="form-control mr-sm-2" type="text" placeholder="Search">
+					<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+				</form> -->
+			</div>
+
+
 		</nav>
 		<div class="overlay sidebar-overlay"></div>
 		<section>
 			<aside id="sidebar-left" class="sidebar">
-				
+				<div class="user-info">
+					<div class="image">
+						<img class="rounded-circle" src="assets/img/alex.jpg" alt="">
+					</div>
+					<div class="info">
+						<h5>Administrator</h5>
+					</div>
+				</div>
 			</aside>
 		</section>
 		
